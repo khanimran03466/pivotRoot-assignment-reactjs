3674e3dac2aa1d81a912847f5d26689a1443b5c1
imran khan <imran.ayub.khan@accenture.com>
2024-03-17 17:02:59 +0530

    initial

--- src/components/MainAd.jsx
+++ src/components/MainAd.jsx
@@ -1,7 +1,7 @@
 import AdBanner from "/images/herobanner-img.png";
 const MainAd = () => {
   return (
-    <div className="main-ad">
+    <div className="main-add">
       <img src={AdBanner} alt="herobanner" />
     </div>
   );
